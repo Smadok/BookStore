b0f57fe231b0f6f06ce33ef319eedb6be66b6811
Lachezar Dimitrov <166111378+Smadok@users.noreply.github.com>
2024-04-20 00:00:52 +0300

    Add files via upload

---
+++ web/web/src/main/java/com/shop/web/services/UserService.java
@@ -0,0 +1,16 @@
+package com.shop.web.services;
+
+import com.shop.web.dto.RegistrationDto;
+import com.shop.web.models.UserEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface UserService {
+    void saveUser(RegistrationDto registrationDto);
+
+    UserEntity findByEmail(String email);
+
+    UserEntity findByUsername(String username);
+
+
+}
